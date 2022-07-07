fd82dc671ff2a86acd697748f3a71b3e67d6da88
namoah23@my.whitworth.edu <namoah23@my.whitworth.edu>
2022-07-06 23:11:40 -0700

    Initial Commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,92 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import * as esbuild from 'esbuild-wasm';
+import { useState, useEffect, useRef} from "react";
+import ReactDOM from "react-dom";
+import { unpkgPathPlugin } from './plugins/unpkg-path-plugin'; 
+import { fetchPlugin } from './plugins/fetch-plugins'; 
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const App = () => { 
+    const ref = useRef<any>(); 
+    const iframe = useRef<any>();
+    const [input, setInput] = useState('');
+    
+
+
+    const startService = async () => {
+        ref.current = await esbuild.startService({
+            worker : true,
+            wasmURL : 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm'
+           
+        });
+        
+    }; 
+
+    useEffect(() => {
+        startService();
+    }, []);
+
+
+    const onClick = async () => {
+        
+        if(!ref.current) { 
+            return;
+        } 
+
+        iframe.current.srcdoc = html; 
+
+        const result = await ref.current.build({
+            entryPoints: ['index.js'],
+            bundle: true, 
+            write: false, 
+            plugins: [
+                unpkgPathPlugin(),
+                fetchPlugin(input),
+            ], 
+            define : {
+                'process.env.NODE_ENV' : ' "production"',
+                global: 'window',
+            }
+        }); 
+
+        // console.log(result);
+
+        //setCode(result.outputFiles[0].text);
+        iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
+
+    };
+    
+    // contents of the iframe. 
+    const html = 
+    ` 
+       <html> 
+        <head> </head>
+        <body> 
+            <div id="root"> </div> 
+            <script> 
+                window.addEventListener('message', (event) => { 
+                    try {
+                        eval(event.data);
+                    } catch (err) {
+                        const root = document.querySelector('#root');
+                        root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
+                    }
+                    
+                }, false); 
+            </script>
+        </body>
+
+       </html>
+    `;
+
+    return <div> 
+        <textarea value={input} onChange={(e) => setInput(e.target.value)}></textarea>
+        <div>
+            <button onClick={onClick}> Submit</button>
+        </div>
+        <iframe title="code Preview" ref={iframe} sandbox="allow-scripts" srcDoc={html} />
+        
+    </div>
+}; 
+
+
+ReactDOM.render(<App />, document.querySelector('#root'));
