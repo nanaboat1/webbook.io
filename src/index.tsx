e5be7b7b03d0966de58a0a6088570260d5b97078
namoah23@my.whitworth.edu <namoah23@my.whitworth.edu>
2022-07-09 00:50:43 -0700

    Monaco Editor Added

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import { useState, useEffect, useRef} from "react";
 import ReactDOM from "react-dom";
 import { unpkgPathPlugin } from './plugins/unpkg-path-plugin'; 
 import { fetchPlugin } from './plugins/fetch-plugins'; 
+import  CodeEditor  from './components/code-editor';
 
 
 const App = () => { 
@@ -79,6 +80,7 @@ const App = () => {
     `;
 
     return <div> 
+        <CodeEditor />
         <textarea value={input} onChange={(e) => setInput(e.target.value)}></textarea>
         <div>
             <button onClick={onClick}> Submit</button>
