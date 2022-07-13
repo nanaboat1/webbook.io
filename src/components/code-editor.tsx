937819f70209ffdf1e0ed89e748eccb2e69a535d
namoah23@my.whitworth.edu <namoah23@my.whitworth.edu>
2022-07-12 18:48:30 -0700

    Refactored iframe

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,7 +1,40 @@
-import MonacoEditor from '@monaco-editor/react'; 
+import MonacoEditor, { OnMount } from '@monaco-editor/react'; 
+import './code-editor.css';
 
-const CodeEditor = () => {
-    return <MonacoEditor language="javascript" height="500px"/>
+
+interface CodeEditorProps { 
+    initialValue: string;
+}
+
+
+const CodeEditor: React.FC<CodeEditorProps> = ({initialValue}) => { 
+
+    
+    const onFormatClick= () => { 
+        // Feature Deprecated
+
+    }
+
+    return (
+    <div className="editor-wrapper">
+        <button className="button button-format isprimary is-small"
+         onClick={onFormatClick}>Format</button>
+    <MonacoEditor 
+    value= {initialValue}
+    theme="vs-dark" 
+    language="javascript"  
+    height="500px"
+    options={{
+        wordWrap: 'on', 
+        minimap : { enabled: false },
+        showUnused : false, // for making sure unused import statements are still used. 
+        folding: false, 
+        lineNumbersMinChars: 3,
+        fontSize: 16, 
+        scrollBeyondLastLine: false,
+        automaticLayout: true, 
+    }}
+    /></div>);
 }; 
 
 export default CodeEditor; 
--- src/index.tsx
+++ src/index.tsx
@@ -4,22 +4,21 @@ import ReactDOM from "react-dom";
 import { unpkgPathPlugin } from './plugins/unpkg-path-plugin'; 
 import { fetchPlugin } from './plugins/fetch-plugins'; 
 import  CodeEditor  from './components/code-editor';
+import 'bulmaswatch/superhero/bulmaswatch.min.css';
+import Preview from './components/preview';
 
 
 const App = () => { 
     const ref = useRef<any>(); 
-    const iframe = useRef<any>();
+    const [code, setCode] = useState('')
     const [input, setInput] = useState('');
+   
     
-
-
     const startService = async () => {
         ref.current = await esbuild.startService({
             worker : true,
             wasmURL : 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm'
-           
-        });
-        
+        });      
     }; 
 
     useEffect(() => {
@@ -32,8 +31,6 @@ const App = () => {
         if(!ref.current) { 
             return;
         } 
-
-        iframe.current.srcdoc = html; 
 
         const result = await ref.current.build({
             entryPoints: ['index.js'],
@@ -49,43 +46,19 @@ const App = () => {
             }
         }); 
 
-        // console.log(result);
-
-        //setCode(result.outputFiles[0].text);
-        iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
+        setCode(result.outputFiles[0].text);
 
     };
     
-    // contents of the iframe. 
-    const html = 
-    ` 
-       <html> 
-        <head> </head>
-        <body> 
-            <div id="root"> </div> 
-            <script> 
-                window.addEventListener('message', (event) => { 
-                    try {
-                        eval(event.data);
-                    } catch (err) {
-                        const root = document.querySelector('#root');
-                        root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
-                    }
-                    
-                }, false); 
-            </script>
-        </body>
-
-       </html>
-    `;
-
+    // Contents of the Webpage.
     return <div> 
-        <CodeEditor />
+        <CodeEditor initialValue='const a =1'  />
         <textarea value={input} onChange={(e) => setInput(e.target.value)}></textarea>
         <div>
             <button onClick={onClick}> Submit</button>
         </div>
-        <iframe title="code Preview" ref={iframe} sandbox="allow-scripts" srcDoc={html} />
+        
+        <Preview code={code} />
         
     </div>
 }; 
